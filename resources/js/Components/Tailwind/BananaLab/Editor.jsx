abe1de1de077cfb469581262068b18ae2ce894b2
Manuel Gamboa <gamboapalominocarlosmanuel@gmail.com>
2025-08-31 17:59:09 -0500

    Agregando pdf lib

--- resources/js/Components/Tailwind/BananaLab/Editor.jsx
+++ resources/js/Components/Tailwind/BananaLab/Editor.jsx
@@ -4639,7 +4639,7 @@ export default function EditorLibro() {
         return container;
     }, [projectData?.canvas_preset, pages, workspaceDimensions])
 
-    const createMirrorImage = useCallback(async (element, targetWidth, outputType = "file", scale = 1.5) => {
+    const createMirrorImage = useCallback(async (element, targetWidth, outputType = "file", scale = 1.2) => {
         if (!projectData?.canvas_preset) return null;
 
         // Guardar estilos originales
